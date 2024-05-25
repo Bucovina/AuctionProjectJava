ead0f5f7ba42ff805f358ea04d888d35a9a49055
stefanbrb10 <35970743+stefanbrb10@users.noreply.github.com>
2024-05-25 14:59:34 +0300

    Menu first implementation

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import classes.menus.MainMenu;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        MainMenu mainMenu = MainMenu.getInstance();
+        Scanner scanner = new Scanner(System.in);
+        MainMenu.inside = true;
+        while (MainMenu.inside) {
+            mainMenu.printMenu();
+            int option = scanner.nextInt();
+            mainMenu.handleMenu(option);
+        }
     }
 }
---
+++ src/classes/menus/BiddersMenu.java
@@ -0,0 +1,37 @@
+package classes.menus;
+
+public class BiddersMenu extends Menu {
+
+        private static BiddersMenu menu = null;
+
+        private BiddersMenu() {
+
+        }
+
+        public static synchronized BiddersMenu getInstance() {
+            if (menu == null) {
+                menu = new BiddersMenu();
+            }
+            return menu;
+        }
+
+        public void printMenu() {
+            System.out.println("----------------- Bidders Menu -----------------");
+            System.out.println("1. Add bidder");
+            System.out.println("2. List bidders");
+            System.out.println("0. Exit");
+        }
+
+        public void handleOption(int option) {
+            switch (option) {
+                case 1:
+                    //bidderAdd();
+                    break;
+                case 2:
+                    //bidderList();
+                    break;
+                default:
+                    System.out.println("Invalid option");
+            }
+        }
+}
